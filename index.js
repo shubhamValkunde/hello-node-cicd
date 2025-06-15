6cedd31fd306d0af8793f18d78ea0ca3cd10e8e1
Shubham <shubham.22110855@viit.ac.in>
2025-06-15 12:17:08 +0530

    Updated hello message

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World from CI/CD pipeline!");
+  res.send(
+    "Hello, World from CI/CD pipeline!, This is the first pipeline created by shubhammusing ai chatgpt"
+  );
 });
 
 app.listen(PORT, () => {
